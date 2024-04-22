604a4144211189210193d8d0898c5519976959f6
nppedronp@outlook.com <nppedronp@outlook.com>
2024-04-21 22:54:23 -0300

    Add snap store install

---
+++ install_scripts/install_from_sanp_store.sh
@@ -0,0 +1,8 @@
+sudo snap install --classic code
+sudo snap install --classic shotcut
+sudo snap install --classic slack
+sudo snap install --classic sublime-text
+sudo snap install --classic nvim
+sudo snap install dbeaver-ce
+sudo snap install discord
+sudo snap install bitwarden
