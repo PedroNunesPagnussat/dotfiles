6a5093b8bfb2eb48637a13940c16e454fd6cc67c
nppedronp@outlook.com <nppedronp@outlook.com>
2024-04-23 00:23:21 -0300

    Symlink creator

--- symlink_creator.sh
+++ symlink_creator.sh
@@ -2,45 +2,47 @@
 set -o errexit
 set -o nounset
 
-
-
-print_header()    { printf "\e[1;37m${@}\e[0m"    ; }
-print_success()   { printf "\e[1;32m[v]\e[0m ${@}"; }
-print_error()     { printf "\e[1;31m[x]\e[0m ${@}"; }
-print_info()      { printf "\e[1;36m[>]\e[0m ${@}"; }
-print_ask()       { printf "\e[1;33m[?]\e[0m ${@}"; }
-
+# Logging functions for colored output
+print_header() { printf "\e[1;37m%s\e[0m\n" "$@"; }
+print_success() { printf "\e[1;32m[v]\e[0m %s\n" "$@"; }
+print_error() {
+	printf "\e[1;31m[x]\e[0m %s\n" "$@"
+	exit 1
+}
+print_info() { printf "\e[1;36m[>]\e[0m %s\n" "$@"; }
+print_ask() { printf "\e[1;33m[?]\e[0m %s" "$@"; }
 
 script_dir() { printf "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"; }
+
 is_root() { [[ "${EUID}" = 0 ]]; }
 
-symlink_file()
-{
-    local src_name="${1}"
-    local src_path="${dotfiles_dir}/${src_name}"
-    local dst_path="${HOME}/.${src_name}"
-    print_info "${src_path}\n"
-    print_info "    Deleting ${dst_path}\n"
-    rm -rf "${dst_path}"
-    print_info "    Linking ${src_path} to ${dst_path}\n"
-    ln -s "${src_path}" "${dst_path}"
-    print_success "    ${src_path} --> ${dst_path}\n"
+symlink_file() {
+	local src_name="${1}"
+	local src_path="${dotfiles_dir}/${src_name}"
+	local dst_path="${HOME}/.${src_name}"
+
+	print_info "Removing existing file: ${dst_path}"
+	rm -rf "${dst_path}"
+
+	print_info "Linking ${src_path} to ${dst_path}"
+
+	ln -s "${src_path}" "${dst_path}"
+
+	print_success "Linked: ${src_path} --> ${dst_path}"
 }
 
+# Main function to orchestrate dotfiles installation
+main() {
+	print_header "Gathering system information..."
+	local dotfiles_dir="$(script_dir)"
+	print_info "Home directory: ${HOME}"
+	print_info "Dotfiles directory: ${dotfiles_dir}"
 
-main()
-{
-    print_header "Gathering system information...\n"
-    declare -r dotfiles_dir="$(script_dir)" 
-    print_info "Home directory: ${HOME}\n"
-    print_info "Dotfiles directory: ${dotfiles_dir}\n"
-    print_header "Symlinking files...\n"
-    
-    symlink_file "bashrc"
-    symlink_file "tmux.conf"
+	print_header "Symlinking files..."
 
+	symlink_file "tmux.conf"
 
-    print_success "Done installing dotfiles!\n"
+	print_success "Done installing dotfiles!"
 }
-main "${@}"
 
+main "$@"
