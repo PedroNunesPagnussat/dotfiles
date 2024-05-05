04229ee6af7edc14eb5b2fcc2d981cf351a1bbac
nppedronp@outlook.com <nppedronp@outlook.com>
2024-05-04 21:27:25 -0300

    Copilot

--- config/nvim/lua/pedro/plugins/comments.lua
+++ config/nvim/lua/pedro/plugins/comments.lua
@@ -21,6 +21,15 @@ return {
       require("Comment.api").toggle.linewise.current()
     end, { desc = "Comment Toggle" })
 
+    map(
+      "v",
+      "<C-/>",
+      "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+      { desc = "Comment Toggle" }
+    )
 
+    map("n", "<C-/>", function()
+      require("Comment.api").toggle.linewise.current()
+    end, { desc = "Comment Toggle" })
   end,
 }
--- config/nvim/lua/pedro/plugins/nvim-cmp.lua
+++ config/nvim/lua/pedro/plugins/nvim-cmp.lua
@@ -1,107 +1,109 @@
 local function border(hl_name)
-	return {
-		{ "╭", hl_name },
-		{ "─", hl_name },
-		{ "╮", hl_name },
-		{ "│", hl_name },
-		{ "╯", hl_name },
-		{ "─", hl_name },
-		{ "╰", hl_name },
-		{ "│", hl_name },
-	}
+  return {
+    { "╭", hl_name },
+    { "─", hl_name },
+    { "╮", hl_name },
+    { "│", hl_name },
+    { "╯", hl_name },
+    { "─", hl_name },
+    { "╰", hl_name },
+    { "│", hl_name },
+  }
 end
 
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-nvim-lua",
-		"hrsh7th/cmp-nvim-lsp",
-		{
-			"L3MON4D3/LuaSnip",
-			-- follow latest release.
-			version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-			-- install jsregexp (optional!).
-			build = "make install_jsregexp",
-		},
-		"saadparwaiz1/cmp_luasnip", -- for autocompletion
-		"rafamadriz/friendly-snippets", -- useful snippets
-		"hrsh7th/cmp-cmdline",
-		"onsails/lspkind.nvim", -- vs-code like pictograms
-	},
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-nvim-lua",
+    "hrsh7th/cmp-nvim-lsp",
+    {
+      "L3MON4D3/LuaSnip",
+      -- follow latest release.
+      version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+      -- install jsregexp (optional!).
+      build = "make install_jsregexp",
+    },
+    "saadparwaiz1/cmp_luasnip", -- for autocompletion
+    "rafamadriz/friendly-snippets", -- useful snippets
+    "hrsh7th/cmp-cmdline",
+    "onsails/lspkind.nvim", -- vs-code like pictograms
+  },
 
-	config = function()
-		local cmp = require("cmp")
+  config = function()
+    local cmp = require("cmp")
 
-		local luasnip = require("luasnip")
+    local luasnip = require("luasnip")
 
-		local lspkind = require("lspkind")
+    local lspkind = require("lspkind")
 
-		require("luasnip.loaders.from_vscode").lazy_load()
+    require("luasnip.loaders.from_vscode").lazy_load()
 
-		cmp.setup({
-			completion = {
-				completeopt = "menu,menuone,preview,noselect",
-			},
+    cmp.setup({
+      completion = {
+        completeopt = "menu,menuone,preview,noselect",
+      },
 
-			window = {
-				documentation = {
-					border = border("CmpDocBorder"),
-					winhighlight = "Normal:CmpDoc",
-				},
-			},
+      window = {
+        documentation = {
+          border = border("CmpDocBorder"),
+          winhighlight = "Normal:CmpDoc",
+        },
+      },
 
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
 
-			mapping = cmp.mapping.preset.insert({
-				["<Tab>"] = cmp.mapping.select_next_item(), -- previous suggestion
-				["<S-Tab>"] = cmp.mapping.select_prev_item(), -- next suggestion
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-				["<C-e>"] = cmp.mapping.abort(), -- close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-			}),
+      mapping = cmp.mapping.preset.insert({
+        -- ["<Tab>"] = cmp.mapping.select_next_item(), -- previous suggestion
+        -- ["<S-Tab>"] = cmp.mapping.select_prev_item(), -- next suggestion
+        ["<C-j>"] = cmp.mapping.select_next_item(), -- previous suggestion
+        ["<C-k>"] = cmp.mapping.select_prev_item(), -- next suggestion
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+        ["<C-e>"] = cmp.mapping.abort(), -- close completion window
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      }),
 
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp", max_item_count = 3, keyword_lenght = 3 },
-				{ name = "buffer", max_item_count = 5 },
-				{ name = "path" },
-				{ name = "nvim_lua" },
-				{ name = "luasnip", max_item_count = 3 },
-			}),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp", max_item_count = 3, keyword_lenght = 3 },
+        { name = "buffer", max_item_count = 3 },
+        { name = "path" },
+        { name = "nvim_lua" },
+        { name = "luasnip", max_item_count = 3 },
+      }),
 
-			-- configure lspkind for vs-code like pictograms in completion menu
-			---@diagnostic disable-next-line: missing-fields
-			formatting = {
-				format = lspkind.cmp_format({
-					maxwidth = 50,
-					ellipsis_char = "...",
-				}),
-			},
-		})
+      -- configure lspkind for vs-code like pictograms in completion menu
+      ---@diagnostic disable-next-line: missing-fields
+      formatting = {
+        format = lspkind.cmp_format({
+          maxwidth = 50,
+          ellipsis_char = "...",
+        }),
+      },
+    })
 
-		-- completion for command mode
-		cmp.setup.cmdline({ "/", "?" }, {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = {
-				{ name = "buffer" },
-			},
-		})
+    -- completion for command mode
+    cmp.setup.cmdline({ "/", "?" }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+      },
+    })
 
-		cmp.setup.cmdline(":", {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = cmp.config.sources({
-				{ name = "path" },
-			}, {
-				{ name = "cmdline" },
-			}),
-		})
-	end,
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+      }, {
+        { name = "cmdline" },
+      }),
+    })
+  end,
 }
