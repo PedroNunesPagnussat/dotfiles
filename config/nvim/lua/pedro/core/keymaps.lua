02cfe01e33d927f8e4da2d90b50ffbd2c816086b
nppedronp@outlook.com <nppedronp@outlook.com>
2024-08-13 23:13:59 -0300

    Congig

--- config/nvim/lua/pedro/core/keymaps.lua
+++ config/nvim/lua/pedro/core/keymaps.lua
@@ -1,105 +1,92 @@
--- Leader
+-- Leader Key
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 local map = vim.keymap.set
 
--- Fix line wrap jk
+-- Navigation with line wrap considerations
 map("n", "j", "gj", { desc = "Move down visual line" })
 map("n", "k", "gk", { desc = "Move up visual line" })
 
--- Copy Buffer
-map({ "n", "v" }, "x", '"_x', { desc = "Don't add x delition to buffer" })
-map("x", "<leader>p", [["_dP]], { desc = "Don't copy to buffer when pasting" })
--- map({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy to Sistem Clipboard" })
--- map("n", "<leader>Y", [["+Y]], { desc = "Copy to Sistem Clipboard" })
+-- Copy and Paste with Clipboard
+map({ "n", "v" }, "x", '"_x', { desc = "Delete without adding to buffer" })
+map({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy to system clipboard" })
+map("n", "<leader>p", [["+p]], { desc = "Paste from system clipboard" })
+map("n", "<leader>P", [["+P]], { desc = "Paste from system clipboard before cursor" })
 
 -- File Operations
-map({ "n", "v" }, "<leader>yp", [[:let @+ = expand("%:p")<cr>]], { desc = "Copy file path to clipboard" })
-map({ "n", "v" }, "<leader>yf", "<cmd>%y+<cr>", { desc = "Copy whole file to clipboard" })
+map("n", "<leader>yp", [[:let @+ = expand("%:p")<cr>]], { desc = "Copy file path to clipboard" })
+map("n", "<leader>yf", "<cmd>%y+<cr>", { desc = "Copy entire file to clipboard" })
 
--- Exit insert mode
+-- Exit Insert Mode
 map("i", "jk", "<ESC>", { desc = "Exit insert mode" })
 map("i", "jj", "<ESC>", { desc = "Exit insert mode" })
 
--- GO to match symbol
-map("n", "gm", "%", { desc = "Go to match symbol" })
+-- Symbol Matching
+map("n", "gm", "%", { desc = "Go to matching symbol" })
 
--- Add ; as :
+-- Command Mode and Yank Enhancements
 map("n", ";", ":", { desc = "Enter command mode" })
-
--- Add Y as y$
 map("n", "Y", "y$", { desc = "Yank to end of line" })
 
--- Map CTRL Z to undo
-map("n", "<C-z>", "<cmd>undo<cr>", { desc = "Undo" })
-map("i", "<C-z>", "<cmd>undo<cr>", { desc = "Undo" })
+-- Undo with Ctrl+Z
+map({ "n", "i" }, "<C-z>", "<cmd>undo<cr>", { desc = "Undo" })
 
--- Better Indenting
-map("v", "<", "<gv")
-map("v", ">", ">gv")
+-- Improved Indentation
+map("v", "<", "<gv", { desc = "Indent left and reselect" })
+map("v", ">", ">gv", { desc = "Indent right and reselect" })
+map("i", "<S-tab>", "<C-d>", { desc = "Un-indent in insert mode" })
 map("v", "<S-tab>", "<gv")
-map("v", "<tab>", ">gv")
-map("i", "<S-tab>", "<C-d>")
 
--- Open VSCode
+-- Open Current Directory in VSCode
 map("n", "<leader>co", "<cmd>!code .<cr>", { desc = "Open in VSCode" })
 
--- Move Lines
-map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
-map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
-map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
-map("n", "<A-down>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
-map("n", "<A-up>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
-map("i", "<A-down>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
-map("i", "<A-up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
-map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "move down" })
-map("v", "<a-k>", ":m '<-2<cr>gv=gv", { desc = "move up" })
-map("v", "<A-down>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
-map("v", "<A-up>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
--- Windows
-map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
-map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
-map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
-map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+-- Line Movement
+map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move line down" })
+map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move line up" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move line down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move line up" })
+map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move selection down" })
+map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move selection up" })
+
+-- Window Navigation and Management
+map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
 
 map("n", "<leader>w|", "<C-w>v", { desc = "Split window vertically" })
 map("n", "<leader>w-", "<C-w>s", { desc = "Split window horizontally" })
 map("n", "<leader>we", "<C-w>=", { desc = "Make windows equal size" })
 map("n", "<leader>wx", "<cmd>close<CR>", { desc = "Close current window" })
 
--- Tabs
+-- Tab Management
 map("n", "<leader><tab>c", "<cmd>tabnew<cr>", { desc = "New Tab" })
 map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
 map("n", "<tab>", "<cmd>tabnext<cr>", { desc = "Next Tab" })
-map("n", "<leader><tab>x", "<cmd>tabclose<cr>", { desc = "Close Tab" })
 map("n", "<s-tab>", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+map("n", "<leader><tab>x", "<cmd>tabclose<cr>", { desc = "Close Tab" })
 
 -- Save File
-map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
--- TODO MAKE A WIRTE ALL
+map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 
 -- New File
 map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
 
--- Find Replace Word
-map("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace Word" })
+-- Find and Replace
+map("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace word under cursor" })
 
--- Incremet / Decrement Number
-map("n", "<leader>+", "<C-a>", { desc = "Increment number" })
-map("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
-
--- Search
-map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Clear hlsearch" })
+-- Search and Clear Highlights
+map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Clear search highlight" })
 
 -- Quit
-map({ "v", "n" }, "<leader>wq", "<cmd>wq<cr>", { desc = "Wirte and quit" })
+map({ "v", "n" }, "<leader>wq", "<cmd>wq<cr>", { desc = "Save and quit" })
 map({ "v", "n" }, "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all windows" })
-map({ "v", "n" }, "<leader>qQ", "<cmd>qa!<cr>", { desc = "Quit all windows" })
+map({ "v", "n" }, "<leader>qQ", "<cmd>qa!<cr>", { desc = "Quit all windows without saving" })
 
--- Cursor QoL
-map("n", "J", "mzJ`z", { desc = "Keep Cursor Fixed on J" })
-map("n", "<C-d>", "<C-d>zz", { desc = "Keep Cursor Fixed on PgUp" })
-map("n", "<C-u>", "<C-u>zz", { desc = "Keep Cursor Fixed on PgDown" })
-map("n", "n", "nzzzv", { desc = "Keep Cursor Fixed on Search" })
-map("n", "N", "Nzzzv", { desc = "Keep Cursor Fixed on Search" })
+-- Cursor Positioning Enhancements
+map("n", "J", "mzJ`z", { desc = "Join lines and keep cursor position" })
+map("n", "<C-d>", "<C-d>zz", { desc = "Keep cursor centered on half-page down" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Keep cursor centered on half-page up" })
+map("n", "n", "nzzzv", { desc = "Keep cursor centered on search result" })
+map("n", "N", "Nzzzv", { desc = "Keep cursor centered on previous search result" })
--- config/nvim/lua/pedro/core/options.lua
+++ config/nvim/lua/pedro/core/options.lua
@@ -2,76 +2,75 @@
 local opt = vim.opt
 
 -- Line Numbers
-opt.relativenumber = true -- Relative line numbers
-opt.number = true -- Absolute line number
+opt.number = true -- Show absolute line number
+opt.relativenumber = true -- Show relative line numbers
 
--- Identation
-opt.tabstop = 2 -- 2 spaces for tabs
-opt.shiftwidth = 2 -- 2 spaces for indent width
-opt.expandtab = true -- Expand tab to spaces
-opt.autoindent = true -- Copy indent from current line on new one
-opt.shiftround = true -- Round indent
+-- Indentation
+opt.tabstop = 2 -- Number of spaces a <Tab> counts for
+opt.shiftwidth = 2 -- Number of spaces to use for each step of (auto)indent
+opt.expandtab = true -- Convert tabs to spaces
+opt.autoindent = true -- Copy indent from current line when starting a new line
+opt.shiftround = true -- Round indent to multiple of 'shiftwidth'
 
 -- Search Settings
-opt.ignorecase = true -- Ignore case when searching
-opt.smartcase = true -- Include mixed case in your search in case-sensitive search
-opt.hlsearch = true -- Set highlight on search
+opt.ignorecase = true -- Ignore case in search patterns
+opt.smartcase = true -- Override 'ignorecase' if search pattern contains uppercase characters
+opt.hlsearch = true -- Highlight matches of previous search pattern
 
 -- Mouse
-opt.mouse = "a" -- Allow Mouse
+opt.mouse = "a" -- Enable mouse support in all modes
 
 -- Backspace
-opt.backspace = "indent,eol,start" -- Allow backspace on indent, end of line or insert mode start position
+opt.backspace = "indent,eol,start" -- Make backspace behave in a more intuitive way
 
 -- Line Wrapping
 opt.wrap = false -- Disable line wrapping
 
 -- Clipboard
-opt.clipboard:append("unnamedplus") -- Use system clipboard as default register
+-- opt.clipboard:append("unnamedplus") -- Use system clipboard as default register
 
 -- Split Windows
-opt.splitright = true -- Split vertical window to the right
-opt.splitbelow = true -- Split horizontal window to the bottom
+opt.splitright = true -- Vertical splits will automatically be to the right
+opt.splitbelow = true -- Horizontal splits will automatically be below
 
 -- Undo
-opt.undofile = true -- Save undo history
-
-local undopath = os.getenv("HOME") .. "/.vim/undodir"
-opt.undodir = undopath
+opt.undofile = true -- Save undo history to an undo file
+opt.undodir = vim.fn.expand("~/.vim/undodir") -- Set undo directory
 
 -- Swap
-opt.swapfile = false
+opt.swapfile = false -- Disable swapfile creation
 
 -- Buffers
-opt.confirm = true -- Confirm before exiting unsaved buffer
+opt.confirm = true -- Ask for confirmation when exiting an unsaved buffer
 
 -- Appearance
-opt.termguicolors = true -- Terminal colors
-opt.background = "dark" -- Colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes" -- Show sign column so that text doesn't shift
-vim.g.have_nerd_font = true -- NerdFont flag
+opt.termguicolors = true -- Enable 24-bit RGB color in the TUI
+opt.background = "dark" -- Use dark background
+opt.signcolumn = "yes" -- Always show the sign column to avoid text shifting
+vim.g.have_nerd_font = true -- Flag indicating the presence of NerdFont
 
--- Scroll off
-opt.scrolloff = 8 -- Minimal number of screen lines to keep above and below the cursor.
+-- Scrolling
+opt.scrolloff = 8 -- Minimum number of lines to keep above and below the cursor
 
 -- Spelling
-opt.spell = false
-opt.spelllang = { "en" }
+opt.spell = false -- Disable spell checking by default
+opt.spelllang = { "en" } -- Set spell check language to English
 
---MD
-opt.conceallevel = 1
+-- Markdown
+opt.conceallevel = 1 -- Enable concealment for Markdown
+
 -- Cursor Line
 opt.cursorline = true -- Highlight the current cursor line
 
--- File explorer
-vim.cmd("let g:netrw_liststyle = 3") -- Set File Explorer to work as a tree
+-- File Explorer (Netrw)
+vim.g.netrw_liststyle = 3 -- Use tree-style listing in Netrw
 
--- Mode
-opt.showmode = false -- Don't show the mode, since it's already in the status line
+-- Mode Display
+opt.showmode = false -- Don't show mode in the command line (assuming a statusline plugin shows it)
 
--- Inisable chars
-opt.list = true -- Show invisable chars
-opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" } -- Change the icons
+-- Inisible Characters
+opt.list = true -- Show invisible characters
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" } -- Customize the display of invisible characters
 
--- Color Line
-opt.colorcolumn = "88" -- Set Color line char
+-- Color Column
+opt.colorcolumn = "79" -- Highlight column 79 for guiding line length
