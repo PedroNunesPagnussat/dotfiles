5d0b75a7867a38db904d9f1a23858b8d21b04c9f
PedroNunesPagnussat <nppedronp@outlook.com>
2024-10-27 15:21:44 -0300

    Test

--- config/nix/flake.nix
+++ config/nix/flake.nix
@@ -5,11 +5,14 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { nixpkgs, ...} @ inputs:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    packages.x86_64-linux.hello = pkgs.hello;
+    packages.x86_64-linux.default = pkgs.hello;
 
   };
 }
