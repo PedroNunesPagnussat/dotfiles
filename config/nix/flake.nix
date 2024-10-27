b6e75d72464c03cd1e3d9da5f30cbefa5bd86b3c
PedroNunesPagnussat <nppedronp@outlook.com>
2024-10-27 15:52:08 -0300

    Test

--- config/nix/flake.nix
+++ config/nix/flake.nix
@@ -1,31 +1,40 @@
 {
-  description = "A very basic flake";
+  description = "Pedro Nix Configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { nixpkgs, ...} @ inputs:
+  outputs = { nixpkgs, ... } @ inputs:
   let
+    # Define pkgs for the x86_64-linux system
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    
+    configuration = { pkgs, ... }:
+    {
+      # All packages that need to be installed system-wide
+      environment.systemPackages = [
+        pkgs.git
+        pkgs.less
+        pkgs.openssh
+        pkgs.ifconfig
+      ];
 
-    # All packages that need to be installed system-wide
-    enviroment.systemPackages = [
-      pkgs.git
-      pkgs.less
-      pkgs.openssh
-    ];
+      # Enable multi-user support (if desired)
+      services.nix-daemon.enable = true;
 
+      # Enable experimental features like flakes
+      nix.settings.experimental-features = ["nix-command" "flakes"];
 
-    # Set default shell to zsh
-    programs.zsh.enable = true;
-
+      # Set default shell to zsh
+      programs.zsh.enable = true;
+    };
 
   in
   {
-
+    # Define packages for x86_64-linux
     packages.x86_64-linux.hello = pkgs.hello;
     packages.x86_64-linux.default = pkgs.hello;
-
   };
 }
+
